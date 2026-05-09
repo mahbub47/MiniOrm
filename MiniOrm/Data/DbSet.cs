68fca3bab356c89a6af8fb94e84b487bd9611ebd
Mahbub Ahmed <94840820+mahbub47@users.noreply.github.com>
2026-05-09 15:18:52 +0600

    doc: documentation added in each file for better understanding

--- MiniOrm/Data/DbSet.cs
+++ MiniOrm/Data/DbSet.cs
@@ -2,6 +2,11 @@
 
 namespace MiniOrm.Data;
 
+/// <summary>
+/// This class represents a set of entities of a specific type, providing methods to query and manipulate the data.
+/// </summary>
+/// <typeparam name="TEntity"></typeparam>
+/// <param name="context"></param>
 public class DbSet<TEntity>(DbContext context) where TEntity : class
 {
     public IEnumerable<Product> ToList()
